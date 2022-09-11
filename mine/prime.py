4da2b5785e3b8fc93c28161e4486c321a0811db0
Tejas <jaybalaji192@gmail.com>
2022-09-11 18:44:01 +0530

    MIne created and pushed

---
+++ mine/prime.py
@@ -0,0 +1,16 @@
+n=int(input("Enter number for table : "))
+
+i=1
+div=0
+
+while i<=n:
+    if n%i==0:
+        div+=1
+    i+=1
+
+print(f"div = {div}")
+
+if div==2:
+    print("prime")
+else:
+    print("Not prime")
