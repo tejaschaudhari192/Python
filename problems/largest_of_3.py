8dc2e2f99c5bb60b8e2f0af2cbc559da1a3e46d0
Tejas Chaudhari <jaybalaji192@gmail.com>
2023-05-21 11:21:44 +0530

    some qns solved

---
+++ problems/largest_of_3.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+num3 = int(input("Enter third number : "))
+
+if num1 > num2 and num1 > num3:
+    print("greatest is " , num1)
+elif num2 > num3:
+    print("greatest is " , num2)
+else:
+    print("greatest is " , num3)
