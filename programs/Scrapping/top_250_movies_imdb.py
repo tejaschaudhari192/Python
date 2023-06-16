b124d74412d371078e43e65a709cc0eecfc1d4c9
Tejas Chaudhari <jaybalaji192@gmail.com>
2023-06-17 00:17:26 +0530

    formatted using black

--- programs/Scrapping/top_250_movies_imdb.py
+++ programs/Scrapping/top_250_movies_imdb.py
@@ -1,27 +1,23 @@
-
 import csv
-csv_file = 'C:/programming/Python/programs/Scrapping/top_250_movies_imdb.csv'
-fields = ['No','Movie']
+
+csv_file = "C:/programming/Python/programs/Scrapping/top_250_movies_imdb.csv"
+fields = ["No", "Movie"]
 rows = []
 
 from bs4 import BeautifulSoup
 import requests
 
-website = 'https://www.imdb.com/chart/top/'
+website = "https://www.imdb.com/chart/top/"
 response = requests.get(website)
 web = response.text
 
-soup = BeautifulSoup(web,'html.parser')
+soup = BeautifulSoup(web, "html.parser")
 
-movies_td = soup.find_all(name='td',class_='titleColumn')
-# for item in movies_td:
-#     print(item.getText)
-movies_a = [movie.find(name='a') for movie in movies_td]
+movies_td = soup.find_all(name="td", class_="titleColumn")
+
+movies_a = [movie.find(name="a") for movie in movies_td]
 movies_list_text = [item.getText() for item in movies_a]
-# movies_list = []
-# for i in movies_list_text:
-#     # movie = 
-#     movies_list.append(i.replace('\n', ''))
+
 print(len(movies_list_text))
 
 no = 1
@@ -31,10 +27,7 @@ for movie in movies_list_text:
     no += 1
 # print(rows)
 
-with open(csv_file, 'w',newline='') as table:
+with open(csv_file, "w", newline="") as table:
     writer = csv.writer(table)
     writer.writerow(fields)
     writer.writerows(rows)
-
-    
-
--- programs/Scrapping/top_40_webseries_imdb.py
+++ programs/Scrapping/top_40_webseries_imdb.py
@@ -1,41 +1,39 @@
-# v1.0 
+# v1.0
 import csv
-csv_file = 'C:/programming/Python/programs/Scrapping/top_40_webseries_imdb.csv'
-fields = ['No','Series']
+
+csv_file = "C:/programming/Python/programs/Scrapping/top_40_webseries_imdb.csv"
+fields = ["No", "Series"]
 rows = []
 
 from bs4 import BeautifulSoup
 import requests
 
-file_path = 'C:/programming/Python/programs/Scrapping/top_40_webseries_imdb.txt'
-website = 'https://www.imdb.com/list/ls026690132/'
+file_path = "C:/programming/Python/programs/Scrapping/top_40_webseries_imdb.txt"
+website = "https://www.imdb.com/list/ls026690132/"
 response = requests.get(website)
 web = response.text
 
-soup = BeautifulSoup(web,'html.parser')
-series_div = soup.find_all(name='div',class_="lister-item-content")
-# for series in series_div:
-#     print(series.find(name='h3'))
-h3_list = [series.find(name='h3') for series in series_div]
-# print(h3_list)
-series_list_text = [text.getText() for text in h3_list]
+soup = BeautifulSoup(web, "html.parser")
+series_div = soup.find_all(name="div", class_="lister-item-content")
 
+h3_list = [series.find(name="h3") for series in series_div]
+
+series_list_text = [text.getText() for text in h3_list]
 
 no = 1
 for text in series_list_text:
-    show_txt = text.replace('\n','')
-    show = show_txt.split('.')
+    show_txt = text.replace("\n", "")
+    show = show_txt.split(".")
     rows.append(show)
     no += 1
     # print(show)
-    
 
-    
-with open(csv_file, 'w',newline='') as table:
+
+with open(csv_file, "w", newline="") as table:
     writer = csv.writer(table)
     writer.writerow(fields)
     writer.writerows(rows)
-    
+
 
 # print(op)
 # with open(file_path, 'w') as f:
