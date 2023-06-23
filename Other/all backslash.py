b612d35a46a4ae7f221eba20760802d03e11739a
Tejas Chaudhari <jaybalaji192@gmail.com>
2023-06-23 18:25:10 +0530

    sound in python

---
+++ Other/all backslash.py
@@ -0,0 +1,15 @@
+import winsound
+import time
+
+frequency = 5000  # Set Frequency To 2500 Hertz
+duration = 400  # Set Duration To 1000 ms == 1 second
+
+# f = 0.1
+# for i in range(500):
+#     winsound.Beep(frequency, duration)
+#     time.sleep(f)
+#     f -= 0.01
+#     print(f)
+#     if f < 0:
+#         break
+import winsound;winsound.Beep(13000, 20000)
